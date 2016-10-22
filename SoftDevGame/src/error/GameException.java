e2e3f289d4b877d59fb6d10ce6e40effd8d59bcc
Michael <mholtmann@ggc.edu>
2016-10-21 20:39:30 -0400

    Added a custom exception class.  Now we can throw are own exceptions.

---
+++ SoftDevGame/src/error/GameException.java
@@ -0,0 +1,19 @@
+package error;
+
+import java.io.IOException;
+
+public class GameException extends IOException
+{
+
+	public GameException()
+	{
+		// TODO Auto-generated constructor stub
+	}
+
+	public GameException(String arg0)
+	{
+		super(arg0);
+		// TODO Auto-generated constructor stub
+	}
+
+}
