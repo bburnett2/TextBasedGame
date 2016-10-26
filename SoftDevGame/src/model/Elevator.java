c8e1fca4783f1ceb12ceea0e49f666919b7fca95
Bess Burnett <bburnett2@ggc.edu>
2016-10-26 09:26:56 -0400

    changed visability

--- SoftDevGame/src/model/Elevator.java
+++ SoftDevGame/src/model/Elevator.java
@@ -3,15 +3,15 @@ package model;
 public class Elevator 
 {
 	private Player player = null;
+	private int Level1 = 3;
+	private int Level2 = 11;
+	private int Level3 = 17;
+	private int Level4 = 21;
+	private int Level5 = 24;
+	private int Level6 = 37;
+	private int Level7 = 44;
+	private int LevelFinal = 46;
 	
-	int Level1 = 3;
-	int Level2 = 11;
-	int Level3 = 17;
-	int Level4 = 21;
-	int Level5 = 24;
-	int Level6 = 37;
-	int Level7 = 44;
-	int LevelFinal = 46;
 	//needs methods to find completed and open floors and print that to user.  Then user will
 	//input which floor.
 	public Elevator(Player player)
